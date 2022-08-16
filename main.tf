d9e9c6c16485abb39ac0015568aec639232e000b
Michael Parker <michael@parker.gg>
2022-08-16 15:01:11 -0500

    feat: consume app-config submodule

--- main.tf
+++ main.tf
@@ -3,50 +3,41 @@ locals {
 }
 
 data "azurerm_resource_group" "resource_group" {
-  name = var.resource_group_name
+  name = var.azure_resource_group_name
 }
 
-resource "azurerm_key_vault" "github_runner_keyvault" {
-  name                = "kv-github-runner${local.name_suffix}"
+resource "azurerm_key_vault" "github_runner_registration_keyvault" {
+  name                = "kv-gh-run-reg${local.name_suffix}"
   location            = data.azurerm_resource_group.resource_group.location
   resource_group_name = data.azurerm_resource_group.resource_group.name
-  tenant_id           = var.tenant_id
+  tenant_id           = var.azure_tenant_id
 
   sku_name = "standard"
 
   soft_delete_retention_days = 7
 }
 
-resource "azurerm_app_configuration" "github_runner_app_config" {
-  name                = "appcs-github-runner${local.name_suffix}"
-  location            = data.azurerm_resource_group.resource_group.location
-  resource_group_name = data.azurerm_resource_group.resource_group.name
+module "app_config" {
+  source = "./modules/app-config"
 
-  sku = "free"
-}
+  name_suffix = local.name_suffix
 
-resource "azurerm_app_configuration_key" "azure_location_config_key" {
-  configuration_store_id = azurerm_app_configuration.github_runner_app_config.id
-  key                    = "azure-location"
-  value                  = data.azurerm_resource_group.resource_group.location
-}
+  azure_app_config_owners = var.azure_app_config_owners
 
-resource "azurerm_app_configuration_key" "azure_resource_group_name_config_key" {
-  configuration_store_id = azurerm_app_configuration.github_runner_app_config.id
-  key                    = "azure-resource-group-name"
-  value                  = data.azurerm_resource_group.resource_group.name
-}
+  azure_registration_key_vault_name = azurerm_key_vault.github_runner_registration_keyvault.name
+  azure_resource_group_location     = data.azurerm_resource_group.resource_group.location
+  azure_resource_group_name         = data.azurerm_resource_group.resource_group.name
+  azure_subnet_id                   = var.azure_subnet_id
+  azure_subscription_id             = var.azure_subscription_id
+  azure_gallery_image_id            = var.azure_gallery_image_id
 
-resource "azurerm_app_configuration_key" "azure_subnet_id_config_key" {
-  configuration_store_id = azurerm_app_configuration.github_runner_app_config.id
-  key                    = "azure-subnet-id"
-  value                  = var.subnet_id
-}
+  github_app_id       = var.github_app_id
+  github_client_id    = var.github_client_id
+  github_organization = var.github_organization
 
-resource "azurerm_app_configuration_key" "azure_subscription_id_config_key" {
-  configuration_store_id = azurerm_app_configuration.github_runner_app_config.id
-  key                    = "azure-subscription-id"
-  value                  = var.subscription_id
+  azure_runner_default_password_key_vault_id = var.azure_runner_default_password_key_vault_id
+  github_client_secret_key_vault_id          = var.github_client_secret_key_vault_id
+  github_private_key_key_vault_id            = var.github_private_key_key_vault_id
 }
 
 // TODO: store some inputs into app config (subscription, tenant, subnet, etc) for use by app service
--- variables.tf
+++ variables.tf
@@ -1,20 +1,52 @@
-variable "tenant_id" {
-  type = string
-}
-
-variable "subscription_id" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
 variable "name_suffix" {
-  type    = string
-  default = ""
+  type = string
 }
 
-variable "subnet_id" {
+variable "azure_tenant_id" {
+  type = string
+}
+
+variable "azure_subscription_id" {
+  type = string
+}
+
+variable "azure_resource_group_name" {
+  type = string
+}
+
+variable "azure_subnet_id" {
+  type = string
+}
+
+variable "azure_gallery_image_id" {
+  type = string
+}
+
+variable "azure_app_config_owners" {
+  type    = list(string)
+  default = []
+}
+
+variable "github_app_id" {
+  type = string
+}
+
+variable "github_client_id" {
+  type = string
+}
+
+variable "github_organization" {
+  type = string
+}
+
+variable "azure_runner_default_password_key_vault_id" {
+  type = string
+}
+
+variable "github_client_secret_key_vault_id" {
+  type = string
+}
+
+variable "github_private_key_key_vault_id" {
   type = string
 }
