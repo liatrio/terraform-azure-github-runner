d22e494290adb1500cca55a1ef72c160159b528d
Michael Parker <michael@parker.gg>
2022-08-16 15:02:06 -0500

    feat: add github octokit code for getting actions runner registration token

---
+++ app/src/github.js
@@ -0,0 +1,48 @@
+const { Octokit } = require("@octokit/core");
+const { createAppAuth } = require("@octokit/auth-app");
+const { getConfigValue, getSecretValue } = require("./azure/config");
+
+let _octokit;
+
+const createOctokit = async () => {
+    const [appId, privateKey, clientId, clientSecret] = await Promise.all([
+        getConfigValue("github-app-id"),
+        getSecretValue("github-private-key"),
+        getConfigValue("github-client-id"),
+        getSecretValue("github-client-secret"),
+    ]);
+
+    return new Octokit({
+        authStrategy: createAppAuth,
+        auth: {
+            installationId: "28110978",
+            appId,
+            privateKey,
+            clientId,
+            clientSecret,
+        }
+    });
+};
+
+const getOctoKit = async () => {
+    if (!_octokit) {
+        _octokit = await createOctokit();
+    }
+
+    return _octokit;
+}
+
+const getRegistrationToken = async () => {
+    const octokit = await getOctoKit();
+    const org = await getConfigValue("github-organization");
+
+    const response = await octokit.request('POST /orgs/{org}/actions/runners/registration-token', {
+        org,
+    });
+
+    console.log("response", response);
+}
+
+module.exports = {
+    getRegistrationToken
+};
