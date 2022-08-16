67a9a006a465fb30805b491fcadd923b3c96700d
Michael Parker <michael@parker.gg>
2022-08-16 15:00:47 -0500

    feat: create app-config terraform module

---
+++ modules/app-config/variables.tf
@@ -0,0 +1,55 @@
+variable "name_suffix" {
+  type = string
+}
+
+variable "azure_resource_group_name" {
+  type = string
+}
+
+variable "azure_resource_group_location" {
+  type = string
+}
+
+variable "azure_subscription_id" {
+  type = string
+}
+
+variable "azure_subnet_id" {
+  type = string
+}
+
+variable "azure_registration_key_vault_name" {
+  type = string
+}
+
+variable "azure_gallery_image_id" {
+  type = string
+}
+
+variable "azure_app_config_owners" {
+  type    = list(string)
+}
+
+variable "github_app_id" {
+  type = string
+}
+
+variable "github_client_id" {
+  type = string
+}
+
+variable "github_organization" {
+  type = string
+}
+
+variable "azure_runner_default_password_key_vault_id" {
+  type = string
+}
+
+variable "github_client_secret_key_vault_id" {
+  type = string
+}
+
+variable "github_private_key_key_vault_id" {
+  type = string
+}
