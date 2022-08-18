82e9d3006a3edcc82150b9d827d9ef5f3e492252
Chris Warren <chrisw@liatrio.com>
2022-08-18 15:57:36 -0500

    feat: set default eviction policy for spot instances (#17)

--- app/src/azure/index.js
+++ app/src/azure/index.js
@@ -79,6 +79,7 @@ const createVM = async (name) => {
                 vmSize
             },
             priority: "Spot",
+            evictionPolicy: "Delete",
             storageProfile: {
                 imageReference: {
                     id: galleryImageId,
