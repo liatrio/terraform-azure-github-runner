21f075e85a13ed05bbb6bd2f51be01fa35f85896
Michael Parker <michael@parker.gg>
2022-08-19 15:11:38 -0500

    feat: implement basic event queue in worker thread

--- app/src/app.js
+++ app/src/app.js
@@ -1,14 +1,44 @@
+import { Worker } from "node:worker_threads";
+
 import Hapi from "@hapi/hapi";
 import Boom from "@hapi/boom";
 import pino from "hapi-pino";
 
-import { reconcile } from "./controller.js";
 import { verifyRequestSignature } from "./crypto.js";
 import { getConfigValue } from "./azure/config.js";
 
 const server = Hapi.server({
     port: 3000,
     host: "0.0.0.0"
+});
+
+await server.register({
+    plugin: pino,
+    options: process.env.NODE_ENV === "production" ? {} : {
+        transport: {
+            target: "pino-pretty"
+        }
+    },
+});
+
+const worker = new Worker("./app/src/worker.js");
+
+worker.on("message", (message) => {
+    if (message.info) {
+        if (message.info.payload) {
+            server.logger.info(message.info.payload, message.info.message);
+        } else {
+            server.logger.info(message.info.message);
+        }
+    }
+
+    if (message.error) {
+        if (message.error.payload) {
+            server.logger.error(message.error.payload, message.error.message);
+        } else {
+            server.logger.error(message.error.message);
+        }
+    }
 });
 
 server.route({
@@ -23,20 +53,23 @@ server.route({
         }
 
         if (request.payload && request.payload.action && request.payload.workflow_job) {
-            await reconcile(request.payload);
+            worker.postMessage(request.payload);
         }
 
         return "ok";
     }
 });
 
-await server.register({
-    plugin: pino,
-    options: process.env.NODE_ENV === "production" ? {} : {
-        transport: {
-            target: "pino-pretty"
-        }
-    },
-});
-
 await server.start();
+
+["SIGINT", "SIGTERM"].forEach((signal) => {
+    process.on(signal, async () => {
+        await server.stop();
+
+        worker.postMessage("stop");
+
+        worker.on("exit", (exitCode) => {
+            process.exit(exitCode);
+        });
+    });
+});
--- app/src/controller.js
+++ app/src/controller.js
@@ -1,10 +1,10 @@
 import {WORKFLOW_QUEUED, WORKFLOW_IN_PROGRESS, WORKFLOW_COMPLETED} from "./constants.js";
 
-export const reconcile = async (event) => {
+export const reconcile = async (logger, event) => {
     // if there's no event, we're running `reconcile` as the controller starts
     // we need to get the current state of the world, and make changes if necessary
     if (!event) {
-
+        return;
     }
 
     // if a workflow is queued, we need to start a new agent to keep our warm pool at the correct size
