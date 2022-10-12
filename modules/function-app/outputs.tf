a455da125df57f40e2b5dc29e8a3ab2eb6eb0138
Mitchell Phillips <100593043+mitchell-liatrio@users.noreply.github.com>
2022-10-12 18:07:35 -0500

    fix: remove anonymous access (#57)

--- modules/function-app/outputs.tf
+++ modules/function-app/outputs.tf
@@ -1,3 +1,3 @@
 output "function_webhook_url" {
-  value = "https://${azurerm_linux_function_app.gh_webhook_event_handler_app.default_hostname}/api/eventHandler"
+  value = nonsensitive("https://${azurerm_linux_function_app.gh_webhook_event_handler_app.default_hostname}/api/eventHandler?clientId=default&code=${data.azurerm_function_app_host_keys.default.default_function_key}")
 }
