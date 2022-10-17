a488e9e30828a6e3e3a2eafc87dba5a0ba6ba29a
Mitchell Phillips <100593043+mitchell-liatrio@users.noreply.github.com>
2022-10-17 16:53:57 -0500

    fix: secure outputs and update reg keyvault (#58)

--- app/runner-controller/logger.js
+++ app/runner-controller/logger.js
@@ -5,7 +5,7 @@ let _logger;
 export const getLogger = () => {
     if (!_logger) {
         _logger = pino({
-            level: process.env.LOG_LEVEL || "debug",
+            level: process.env.LOG_LEVEL || "info",
             transport: process.env.NODE_ENV === "production"
                 ? undefined
                 : { target: "pino-pretty",
--- modules/function-app/outputs.tf
+++ modules/function-app/outputs.tf
@@ -1,3 +1,4 @@
 output "function_webhook_url" {
-  value = nonsensitive("https://${azurerm_linux_function_app.gh_webhook_event_handler_app.default_hostname}/api/eventHandler?clientId=default&code=${data.azurerm_function_app_host_keys.default.default_function_key}")
+  sensitive = true
+  value     = "https://${azurerm_linux_function_app.gh_webhook_event_handler_app.default_hostname}/api/eventHandler?clientId=default&code=${data.azurerm_function_app_host_keys.default.default_function_key}"
 }
