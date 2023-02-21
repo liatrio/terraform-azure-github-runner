cbe50123dc15b703ce4e70052f27799ef056e86c
Steven O <steveno@liatrio.com>
2023-02-21 17:36:36 -0600

    missed output

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "custom_data_script" {
   value = module.app_config.custom_data_script
 }
 
-output "function_webhook_url" {
-  sensitive = true
-  value     = module.github_webhook_event_handler_function_app.function_webhook_url
-}
+#output "function_webhook_url" {
+#  sensitive = true
+#  value     = module.github_webhook_event_handler_function_app.function_webhook_url
+#}
